5c3347bb763f1111a52e15217b6070e1adbf4bf2
officer <kasaiso@amazon.com>
2019-10-29 18:37:53 +0000

    add A record

--- main.tf
+++ main.tf
@@ -48,3 +48,15 @@ resource "aws_lb_listener" "ssl" {
     }
   }
 }
+
+# A Record
+resource "aws_route53_record" "record" {
+  zone_id = "${var.zone_id}"
+  name    = "${var.domain_name}"
+  type    = "A"
+  alias {
+    name                    = "${aws_lb.alb.dns_name}"
+    zone_id                 = "${aws_lb.alb.zone_id}"
+    evaluate_target_health  = true
+  }
+}
